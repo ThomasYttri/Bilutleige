60cac212d3bd7418789e3ff4dc4f63af8a5d1894
Thomas Valvik Yttri <thomasvalvikyttri@Thomass-MacBook-Pro.local>
2020-02-15 02:47:29 +0100

    Update Entities

--- src/Entities/Utleigekontor.java
+++ src/Entities/Utleigekontor.java
@@ -1,8 +1,28 @@
 package Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Author Thomas V.Yttri
  */
 public class Utleigekontor{
+    private static final AtomicInteger teller = new AtomicInteger(0);
+    private int kontornummer;
+    private Adresse adresse;
+    private long telefon;
+    private List<Bil> biler;
 
+    /**
+     * Constructor Utleigekontor
+     * @param adresse
+     * @param telefon
+     */
+    public Utleigekontor(Adresse adresse, long telefon) {
+        kontornummer = teller.incrementAndGet();
+        this.adresse = adresse;
+        this.telefon = telefon;
+        this.biler = new ArrayList<>();
+    }
 }
